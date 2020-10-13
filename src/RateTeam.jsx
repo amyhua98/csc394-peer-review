65f180ca32ada326d51cdc3a45c4818b9652ba4f
Amy Hua <aymay98@gmail.com>
2020-10-12 20:06:39 -0500

    Added tabs and icons

--- src/RateTeam.jsx
+++ src/RateTeam.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const rateTeam = () => {
-  return <div>This is the Rate Team page</div>;
+  return <div><h1>Rate Teams</h1>This is the Rate Team page</div>;
 };
 
 export default rateTeam;

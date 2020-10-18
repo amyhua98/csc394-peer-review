be7f7e93682d92330933bf0a4dd188e65a73cb07
Amy Hua <aymay98@gmail.com>
2020-10-18 13:15:50 -0500

    Changed Rate pages

--- src/RateStudent.jsx
+++ src/RateStudent.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const rateStudent = () => {
-  return <div><h1>Rate Students</h1>This is the Rate Student page</div>;
+  return <div><h1>Rate Students</h1><h2>This is the Rate Student page</h2>
+
+  </div>;
 };
 
 export default rateStudent;
--- src/RateTeam.jsx
+++ src/RateTeam.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import './App.css';
 
-const rateTeam = () => {
-  return <div><h1>Rate Teams</h1>This is the Rate Team page</div>;
+const rateTeam = (
+  
+) => {
+  return <div><h1>Rate Teams</h1>
+  <h2>This is the Rate Team page</h2>
+
+<body>
+ <div class="rate">
+    <input type="radio" id="star5" name="rate" value="5" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="4" />
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="3" />
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="2" />
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="1" />
+    <label for="star1" title="text">1 star</label>
+  </div>
+</body>
+
+  </div>
+
+
+
+  ;
 };
 
 export default rateTeam;
+
+
